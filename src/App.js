0311b18481d71f92c069dab866862410ee2fcb1a
David B <144248533+ccDavidbla@users.noreply.github.com>
2024-03-21 16:17:12 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./stile.css";
 
-function App() {
+const App = () => {
+  const [color, setColor] = useState("");
+  const [size, setSize] = useState(100);
+  const [cuadros, setCuadros] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!color || isNaN(size)) {
+      return;
+    }
+    const nuevoCuadro = {
+      color,
+      size,
+    };
+    setCuadros([...cuadros, nuevoCuadro]);
+    setColor("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Agregar cuadro</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="color">Color:</label>
+        <input
+          type="text"
+          id="color"
+          name="color"
+          value={color}
+          onChange={(event) => setColor(event.target.value)}
+        />
+        <br />
+        <label htmlFor="size">Tamaño (px):</label>
+        <input
+          type="number"
+          id="size"
+          name="size"
+          value={size}
+          onChange={(event) => setSize(parseInt(event.target.value))}
+        />
+        <br />
+        <button type="submit">Agregar</button>
+      </form>
+      <div className="cuadros">
+        {cuadros.map((cuadro) => (
+          <div
+            key={cuadro.color}
+            style={{
+              backgroundColor: cuadro.color,
+              width: `${cuadro.size}px`,
+              height: `${cuadro.size}px`,
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
